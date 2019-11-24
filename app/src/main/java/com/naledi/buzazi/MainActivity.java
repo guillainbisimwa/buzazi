03456578db6180dfeccdf98cf5c9ecb523d1bcbc
guillain bisimwa <guillainbisimwa@hotmail.fr>
2019-11-24 01:58:31 -0500

    Initial commit

---
+++ app/src/main/java/com/naledi/buzazi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.naledi.buzazi;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_web_view);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //REMOTE RESOURCE
+        mWebView.loadUrl("https://buzazi.com");
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/naledi/buzazi/Presentation.java
@@ -0,0 +1,69 @@
+package com.naledi.buzazi;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Presentation extends  AhoyOnboarderActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("Prévention", "Prévenir les risques sanitaires liés à la grossesse chez les femmes.", R.drawable.bun);
+        AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Reduction", "Réduire la mortalité infantile.", R.drawable.btrois);
+        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("Contribution", "Contribuer à prévenir le VIH/sida.", R.drawable.bdeux);
+
+        ahoyOnboarderCard1.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard2.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard3.setBackgroundColor(R.color.black_transparent);
+
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+
+        pages.add(ahoyOnboarderCard1);
+        pages.add(ahoyOnboarderCard2);
+        pages.add(ahoyOnboarderCard3);
+
+        for (AhoyOnboarderCard page : pages) {
+            page.setTitleColor(R.color.white);
+            page.setDescriptionColor(R.color.grey_200);
+            //page.setTitleTextSize(dpToPixels(12, this));
+            //page.setDescriptionTextSize(dpToPixels(8, this));
+            //page.setIconLayoutParams(width, height, marginTop, marginLeft, marginRight, marginBottom);
+        }
+
+        setFinishButtonTitle("Lancer");
+        showNavigationControls(true);
+        setGradientBackground();
+
+        //set the button style you created
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
+        }
+
+        //Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        //setFont(face);
+
+        setOnboardPages(pages);
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+        //Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
+        //MainActivity()
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
